13678a88aa93c24c0fdd14827651f017de5a11c3
Temirlan504 <temirlan.ergazy@gmail.com>
2022-12-30 22:15:24 -0500

    Modified  main.py and added level.py file
    level.py file contains instructions and all the logic for the game. main.py runs level.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pygame, sys
+from level import Level
 
 pygame.init()
 
-WIDTH, HEIGHT = 1280, 720
-display = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Laika")
-clock = pygame.time.Clock()
+WIDTH, HEIGHT = 1280, 720 # Screen width and height
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) # Creating screen window
+pygame.display.set_caption("Laika") # Name of the window (game)
+clock = pygame.time.Clock() # Clock for the game run
+
+level = Level() # game itself
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
     dt = clock.tick()/1000
+
+    level.run(dt) # Running game
+
     pygame.display.update()
