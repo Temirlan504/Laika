562a130e43925197a23035616cae1f5c9fa4223c
Temirlan504 <temirlan.ergazy@gmail.com>
2023-01-01 23:19:21 -0500

    Added player movement
    Added player movement with correct diagonal movement

--- level.py
+++ level.py
@@ -5,13 +5,13 @@ from settings import WIDTH, HEIGHT
 class Level:
     def __init__(self, display_surface):
         self.display_surface = display_surface
-        # Sprite groups
-        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites = pygame.sprite.Group() # Sprite group for everything
 
         # Player
         self.player = Player((WIDTH/2, HEIGHT/2), self.all_sprites)
 
     def run(self, dt):
         self.display_surface.fill("black")
-        self.all_sprites.draw(self.display_surface)
-        self.all_sprites.update()
+
+        self.all_sprites.draw(self.display_surface) # Draw all the sprites
+        self.all_sprites.update(dt) # Update all the methods inside every sprite class
--- player.py
+++ player.py
@@ -6,3 +6,42 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32,32))
         self.image.fill("white")
         self.rect = self.image.get_rect(center=pos)
+
+        # Movement attributes
+        self.direction = pygame.math.Vector2() # Will determine the direction of the player's movement in "x" and "y"
+        self.pos = pygame.math.Vector2(self.rect.center) # store position as float
+        self.speed = 200
+
+    # Check if any key is pressed
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.direction.y = -1 # Direction is up
+        elif keys[pygame.K_s]:
+            self.direction.y = 1 # Direction is down
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_a]:
+            self.direction.x = -1 # Direction is left
+        elif keys[pygame.K_d]:
+            self.direction.x = 1 # Direction is right
+        else:
+            self.direction.x = 0
+
+    def move_player(self, dt):
+        if self.direction.magnitude() > 0: # Handling zero magnitude error
+            self.direction = self.direction.normalize() # Make sure that the vector is same in diagonal movement (.noramlize() is pygame's function)
+
+        # Horizontal movement
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+
+        # Vertical movement
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
+
+    def update(self, dt):
+        self.get_input()
+        self.move_player(dt)
