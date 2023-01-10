d9e13c35013344bd36339d5c273e9453f94cf37a
Temirlan504 <temirlan.ergazy@gmail.com>
2023-01-09 21:58:24 -0500

    Added player-centered camera system
    Coded player-centered camera that follows player.
    
    Also, added Trees for testing purposes (to be deleted in the future)

--- camera.py
+++ camera.py
@@ -5,8 +5,18 @@ class CameraGroup(pygame.sprite.Group):
         super().__init__()
         # Get the display_surface that was created from pygame.display.set_mode(...)
         self.display_surface = pygame.display.get_surface()
+        self.offset = pygame.math.Vector2()
+        self.half_w = self.display_surface.get_size()[0] // 2
+        self.half_h = self.display_surface.get_size()[1] // 2
+
+    def center_target_cam(self, target):
+        self.offset.x = target.rect.centerx - self.half_w
+        self.offset.y = target.rect.centery - self.half_h
 
     # Method to custom draw all the sprites (same as sprites.draw() but mine)
-    def custom_draw(self):
+    def custom_draw(self, player):
+        self.center_target_cam(player)
+
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
-            self.display_surface.blit(sprite.image, sprite.rect)
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
--- level.py
+++ level.py
@@ -2,6 +2,16 @@ import pygame
 from player import Player
 from camera import CameraGroup
 from settings import WIDTH, HEIGHT
+from random import randint
+
+# ====================
+class Tree(pygame.sprite.Sprite):
+    def __init__(self,pos,group):
+        super().__init__(group)
+        self.image = pygame.Surface((32,64))
+        self.image.fill("black")
+        self.rect = self.image.get_rect(topleft=pos)
+# ====================
 
 class Level:
     def __init__(self, display_surface):
@@ -12,9 +22,17 @@ class Level:
         # Create a player object and place it in the center and add it to the camera_group sprites group
         self.player = Player((WIDTH/2, HEIGHT/2), self.camera_group)
 
+        # ====================
+        # Spawning trees
+        for i in range(20):
+            rand_x = randint(0,1000)
+            rand_y = randint(0,1000)
+            Tree((rand_x, rand_y), self.camera_group)
+        # ====================
+
     def run(self, dt):
         self.display_surface.fill((115, 68, 46)) # Fill screen with brown-ish colour
 
         # Drawing sprites
-        self.camera_group.custom_draw() # Draw all the sprites on the display surface
+        self.camera_group.custom_draw(self.player) # Draw all the sprites on the display surface
         self.camera_group.update(dt) # Call update methods of all sprites
